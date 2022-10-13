dc69017e4fd05eb77b84af8c52c531dcac47ec81
greyson-wells <71652151+greyson-wells@users.noreply.github.com>
2022-10-13 09:11:41 -0400

    Update __init__.py

--- mario/__init__.py
+++ mario/__init__.py
@@ -6,7 +6,7 @@ def exists():
   """mario.py exists"""
   check50.exists("mario.py")
   
-@check50.checks(exists)
+@check50.check(exists)
 def test1():
   """input of 1 yields correct output"""
   output = check50.run("python3 mario.py").stdin("1", prompt=False).stdout(r'\$?#', '#').exit()
