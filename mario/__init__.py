f3b3f95b5eea1a81b73518dd42071671d49ba929
greyson-wells <71652151+greyson-wells@users.noreply.github.com>
2022-10-17 21:01:56 -0400

    Update __init__.py

--- mario/__init__.py
+++ mario/__init__.py
@@ -22,14 +22,52 @@ def test2():
   output = check50.run("python3 mario.py").stdin("2").stdout()
   if not output == open("2.txt").read():
     raise check50.Failure("did not correctly build pyramid of height 2")
+    
+@check50.check(exists)
+def test5():
+  """input of 5 yields correct output"""
+  output = check50.run("python3 mario.py").stdin("5").stdout()
+  if not output == open("5.txt").read():
+    raise check50.Failure("did not correctly build pyramid of height 5")
+    
+@check50.check(exists)
+def test8():
+  """input of 8 yields correct output"""
+  output = check50.run("python3 mario.py").stdin("8").stdout()
+  if not output == open("8.txt").read():
+    raise check50.Failure("did not correctly build pyramid of height 8")
+    
+@check50.check(exists)
+def test0():
+  """rejects a height of 0"""
+  output = check50.run("python3 mario.py").stdin("0").stdout()
+  if not output == "":
+    raise check50.Failure("program did not reject invalid input")
+    
+@check50.check(exists)
+def test9():
+  """rejects a height of 9"""
+  output = check50.run("python3 mario.py").stdin("9").stdout()
+  if not output == "":
+    raise check50.Failure("program did not reject invalid input")
   
 @check50.check(exists)
 def test_negative_two():
   """rejects a height of -2"""
-  output = check50.run("python3 mario.py").stdin("-1").stdout()
+  output = check50.run("python3 mario.py").stdin("-2").stdout()
   if not output == "":
     raise check50.Failure("program did not reject invalid input")
 
-def checkPyramid(output, correct):
-  if output == correct:
-    return
+@check50.check(exists)
+def test_foo():
+  """rejects a non-numeric height of "foo" """
+  output = check50.run("python3 mario.py").stdin("foo").stdout()
+  if not output == "":
+    raise check50.Failure("program did not reject invalid input")
+    
+@check50.check(exists)
+def test_empty():
+  """rejects a non-numeric height of "" """
+  output = check50.run("python3 mario.py").stdin("").stdout()
+  if not output == "":
+    raise check50.Failure("program did not reject invalid input")
