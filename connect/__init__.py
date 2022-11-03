ead07059f696a5a78b481ff238a2b52344e7ad5b
greyson-wells <71652151+greyson-wells@users.noreply.github.com>
2022-11-03 14:57:45 -0400

    Create __init__.py

---
+++ connect/__init__.py
@@ -0,0 +1,14 @@
+import check50
+import re 
+
+@check50.check()
+def exists():
+  """mario.py exists"""
+  check50.exists("mario.py")
+  check50.include("p1win1.txt", "p1win2.txt", "p1win3.txt", "p1win4.txt", "p2win1.txt", "p2win2.txt", "p2win3.txt", "p2win4.txt")
+  
+@check50.check(exists)
+def test_player1_condition1():
+  """player 1 wins with a horizontal line"""
+  input = open("p1win1.txt").read
+  check50.run("python connect.py").stdin(input).stdout("P1 WINS")
