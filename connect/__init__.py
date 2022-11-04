8b6cc1b510fc1738e4da021746e61477a75ddca8
greyson-wells <71652151+greyson-wells@users.noreply.github.com>
2022-11-04 09:47:01 -0400

    Update __init__.py

--- connect/__init__.py
+++ connect/__init__.py
@@ -9,39 +9,39 @@ def exists():
 @check50.check(exists)
 def test_player1_condition1():
   """player 1 wins with a horizontal line"""
-  check50.run("python connect.py").stdin("p1").stdin("p2").stdin("1").stdin("7").stdin("2").stdin("6").stdin("3").stdin("5").stdin("4").stdout("P1 WINS")
+  check50.run("python connect.py").stdin("p1").stdin("p2").stdin("1").stdin("7").stdin("2").stdin("6").stdin("3").stdin("5").stdin("4").stdout("p1 WINS")
   
 @check50.check(exists)
 def test_player1_condition2():
   """player 1 wins with a vertical line"""
-  check50.run("python connect.py").stdin("p1").stdin("p2").stdin("1").stdin("2").stdin("1").stdin("2").stdin("1").stdin("2").stdin("1").stdout("P1 WINS")
+  check50.run("python connect.py").stdin("p1").stdin("p2").stdin("1").stdin("2").stdin("1").stdin("2").stdin("1").stdin("2").stdin("1").stdout("p1 WINS")
   
 @check50.check(exists)
 def test_player1_condition3():
   """player 1 wins with a bottom left to top right diagonal line"""
-  check50.run("python connect.py").stdin("p1").stdin("p2").stdin("1").stdin("2").stdin("2").stdin("3").stdin("3").stdin("4").stdin("3").stdin("4").stdin("4").stdin("7").stdin("4").stdout("P1 WINS")
+  check50.run("python connect.py").stdin("p1").stdin("p2").stdin("1").stdin("2").stdin("2").stdin("3").stdin("3").stdin("4").stdin("3").stdin("4").stdin("4").stdin("7").stdin("4").stdout("p1 WINS")
   
 @check50.check(exists)
 def test_player1_condition4():
   """player 1 wins with a top left to bottom right diagonal line"""
-  check50.run("python connect.py").stdin("p1").stdin("p2").stdin("1").stdin("1").stdin("4").stdin("1").stdin("1").stdin("3").stdin("3").stdin("2").stdin("6").stdin("2").stdin("2").stdout("P1 WINS")
+  check50.run("python connect.py").stdin("p1").stdin("p2").stdin("1").stdin("1").stdin("4").stdin("1").stdin("1").stdin("3").stdin("3").stdin("2").stdin("6").stdin("2").stdin("2").stdout("p1 WINS")
 
 @check50.check(exists)
 def test_player2_condition1():
   """player 2 wins with a horizontal line"""
-  check50.run("python connect.py").stdin("p1").stdin("p2").stdin("7").stdin("1").stdin("7").stdin("2").stdin("6").stdin("3").stdin("5").stdin("4").stdout("P2 WINS")
+  check50.run("python connect.py").stdin("p1").stdin("p2").stdin("7").stdin("1").stdin("7").stdin("2").stdin("6").stdin("3").stdin("5").stdin("4").stdout("p2 WINS")
   
 @check50.check(exists)
 def test_player2_condition2():
   """player 2 wins with a vertical line"""
-  check50.run("python connect.py").stdin("p1").stdin("p2").stdin("7").stdin("1").stdin("2").stdin("1").stdin("2").stdin("1").stdin("2").stdin("1").stdout("P2 WINS")
+  check50.run("python connect.py").stdin("p1").stdin("p2").stdin("7").stdin("1").stdin("2").stdin("1").stdin("2").stdin("1").stdin("2").stdin("1").stdout("p2 WINS")
   
 @check50.check(exists)
 def test_player2_condition3():
   """player 2 wins with a bottom left to top right diagonal line"""
-  check50.run("python connect.py").stdin("p1").stdin("p2").stdin("7").stdin("1").stdin("2").stdin("2").stdin("3").stdin("3").stdin("4").stdin("3").stdin("4").stdin("4").stdin("7").stdin("4").stdout("P2 WINS")
+  check50.run("python connect.py").stdin("p1").stdin("p2").stdin("7").stdin("1").stdin("2").stdin("2").stdin("3").stdin("3").stdin("4").stdin("3").stdin("4").stdin("4").stdin("7").stdin("4").stdout("p2 WINS")
 
 @check50.check(exists)
 def test_player2_condition4():
   """player 2 wins with a top left to bottom right diagonal line"""
-  check50.run("python connect.py").stdin("p1").stdin("p2").stdin("7").stdin("1").stdin("1").stdin("4").stdin("1").stdin("1").stdin("3").stdin("3").stdin("2").stdin("6").stdin("2").stdin("2").stdout("P2 WINS")
+  check50.run("python connect.py").stdin("p1").stdin("p2").stdin("7").stdin("1").stdin("1").stdin("4").stdin("1").stdin("1").stdin("3").stdin("3").stdin("2").stdin("6").stdin("2").stdin("2").stdout("p2 WINS")
