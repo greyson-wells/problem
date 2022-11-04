6995aa577ae042d426e60558b876eeecee744b60
greyson-wells <71652151+greyson-wells@users.noreply.github.com>
2022-11-04 09:16:43 -0400

    Update __init__.py

--- connect/__init__.py
+++ connect/__init__.py
@@ -5,16 +5,14 @@ import re
 def exists():
   """connect.py exists"""
   check50.exists("connect.py")
-  check50.include("p1win1.txt", "p1win2.txt", "p1win3.txt", "p1win4.txt", "p2win1.txt", "p2win2.txt", "p2win3.txt", "p2win4.txt")
 
-@check50.check(exists)
-def test():
-  """testing"""
-  check50.run("python connect.py").stdin("p1").stdin("p2").stdin("1").stdin("7").stdin("2").stdin("6").stdin("3").stdin("5").stdin("4").stdout("P1 WINS")
-  
 @check50.check(exists)
 def test_player1_condition1():
   """player 1 wins with a horizontal line"""
-  input = open("p1win1.txt").read()
-  check50.run("python connect.py").stdin(input).stdout("P1 WINS")
+  check50.run("python connect.py").stdin("p1").stdin("p2").stdin("1").stdin("7").stdin("2").stdin("6").stdin("3").stdin("5").stdin("4").stdout("P1 WINS")
+  
+@check50.check(exists)
+def test_player1_condition2():
+  """player 1 wins with a bottom left to top right diagonal line"""
+  check50.run("python connect.py").stdin("p1").stdin("p2").stdin("1").stdin("2").stdin("2").stdin("3").stdin("3").stdin("4").stdin("3").stdin("4").stdin("4").stdin("7").stdin("4").stdout("P1 WINS")
   
