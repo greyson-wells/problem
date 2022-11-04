dd3a2faed9282c5231636be307d1ab55c6477276
greyson-wells <71652151+greyson-wells@users.noreply.github.com>
2022-11-03 20:24:29 -0400

    Update __init__.py

--- connect/__init__.py
+++ connect/__init__.py
@@ -6,6 +6,12 @@ def exists():
   """connect.py exists"""
   check50.exists("connect.py")
   check50.include("p1win1.txt", "p1win2.txt", "p1win3.txt", "p1win4.txt", "p2win1.txt", "p2win2.txt", "p2win3.txt", "p2win4.txt")
+
+@check50.check()
+def test(exists):
+  """testing"""
+  input = open("p1win2.txt").read()
+  print(input)
   
 @check50.check(exists)
 def test_player1_condition1():
